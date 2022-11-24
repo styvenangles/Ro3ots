f17eac0523bb9227dfe5e3ef2474db7538697137
Styven Angles <styven.angles@ynov.com>
2022-11-24 14:10:31 +0100

    Ajout RightClick cpp

---
+++ Source/Ro3ots/Ro3otsPlayerController.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ro3otsPlayerController.h"
+#include "GameFramework/Pawn.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Ro3otsCharacter.h"
+#include "Engine/World.h"
+
+ARo3otsPlayerController::ARo3otsPlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
+
+void ARo3otsPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	if (bInputPressed)
+	{
+		FollowTime += DeltaTime;
+
+		// Look for the touch location
+		FVector HitLocation = FVector::ZeroVector;
+		FHitResult Hit;
+		if (bIsTouch)
+		{
+			GetHitResultUnderFinger(ETouchIndex::Touch1, ECC_Visibility, true, Hit);
+		}
+		else
+		{
+			GetHitResultUnderCursor(ECC_Visibility, true, Hit);
+		}
+		HitLocation = Hit.Location;
+
+		// Direct the Pawn towards that location
+		APawn* const MyPawn = GetPawn();
+		if (MyPawn)
+		{
+			FVector WorldDirection = (HitLocation - MyPawn->GetActorLocation()).GetSafeNormal();
+			MyPawn->AddMovementInput(WorldDirection, 1.f, false);
+		}
+	}
+	else
+	{
+		FollowTime = 0.f;
+	}
+}
+
+void ARo3otsPlayerController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("SetDestination", IE_Pressed, this, &ARo3otsPlayerController::OnSetDestinationPressed);
+	InputComponent->BindAction("SetDestination", IE_Released, this, &ARo3otsPlayerController::OnSetDestinationReleased);
+
+	// support touch devices 
+	InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ARo3otsPlayerController::OnTouchPressed);
+	InputComponent->BindTouch(EInputEvent::IE_Released, this, &ARo3otsPlayerController::OnTouchReleased);
+
+}
+
+void ARo3otsPlayerController::OnSetDestinationPressed()
+{
+	// We flag that the input is being pressed
+	bInputPressed = true;
+	// Just in case the character was moving because of a previous short press we stop it
+	StopMovement();
+}
+
+void ARo3otsPlayerController::OnSetDestinationReleased()
+{
+	// Player is no longer pressing the input
+	bInputPressed = false;
+
+	// If it was a short press
+	if (FollowTime <= ShortPressThreshold)
+	{
+		// We look for the location in the world where the player has pressed the input
+		FVector HitLocation = FVector::ZeroVector;
+		FHitResult Hit;
+		GetHitResultUnderCursor(ECC_Visibility, true, Hit);
+		HitLocation = Hit.Location;
+
+		// We move there and spawn some particles
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, HitLocation);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, HitLocation, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
+	}
+}
+
+void ARo3otsPlayerController::OnTouchPressed(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	bIsTouch = true;
+	OnSetDestinationPressed();
+}
+
+void ARo3otsPlayerController::OnTouchReleased(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	bIsTouch = false;
+	OnSetDestinationReleased();
+}
---
+++ Source/Ro3ots/Ro3otsPlayerController.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Ro3otsPlayerController.generated.h"
+
+/**
+ * 
+ */
+ /** Forward declaration to improve compiling times */
+class UNiagaraSystem;
+
+UCLASS()
+class RO3OTS_API ARo3otsPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ARo3otsPlayerController();
+
+	/** Time Threshold to know if it was a short press */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+		float ShortPressThreshold;
+
+	/** FX Class that we will spawn when clicking */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+		UNiagaraSystem* FXCursor;
+
+protected:
+	/** True if the controlled character should navigate to the mouse cursor. */
+	uint32 bMoveToMouseCursor : 1;
+
+	// Begin PlayerController interface
+	virtual void PlayerTick(float DeltaTime) override;
+	virtual void SetupInputComponent() override;
+	// End PlayerController interface
+
+	/** Input handlers for SetDestination action. */
+	void OnSetDestinationPressed();
+	void OnSetDestinationReleased();
+	void OnTouchPressed(const ETouchIndex::Type FingerIndex, const FVector Location);
+	void OnTouchReleased(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+private:
+	bool bInputPressed; // Input is bring pressed
+	bool bIsTouch; // Is it a touch device
+	float FollowTime; // For how long it has been pressed
+
+};
